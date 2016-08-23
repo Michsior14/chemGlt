58b4b8f6493e7a2dc1c7512a3b509efc61fbf863
Viktoria <Viktoria@pc22.home>
2016-08-23 16:17:51 +0200

    REFACTOR: selectable left menu with router

---
+++ imports/ui/helpers/SelectableListWraper.jsx
@@ -0,0 +1,44 @@
+import Component from "react";
+import PropTypes from "react";
+
+function SelectableListWrapper(ComposedComponent) {
+    const propTypes = {
+        children: PropTypes.node.isRequired,
+        defaultValue: PropTypes.number.isRequired,
+    };
+
+    const toReturn = class extends Component {
+        constructor(props){
+            super(props);
+            this.handleRequestChange = this.handleRequestChange.bind(this);
+        }
+
+        componentWillMount() {
+            this.setState({
+                selectedIndex: this.props.defaultValue,
+            });
+        }
+
+        handleRequestChange(event, index) {
+            this.setState({
+                selectedIndex: index,
+            });
+        };
+
+        render() {
+            return (
+                <ComposedComponent
+                    value={this.state.selectedIndex}
+                    onChange={this.handleRequestChange}
+                >
+                    {this.props.children}
+                </ComposedComponent>
+            );
+        }
+    };
+    toReturn.PropTypes = propTypes;
+
+    return toReturn;
+}
+
+export default SelectableListWrapper;
