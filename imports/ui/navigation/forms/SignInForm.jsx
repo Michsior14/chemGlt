5563dda19f0b744dbf558b590c07c84723231a98
Michał Mrozek <michal.mrozek@abiserwkom.eu>
2016-08-27 01:03:47 +0200

    REFACTOR: login inside form, added validations and messages (todo one container with - validations and messages)

--- imports/ui/navigation/forms/SignInForm.jsx
+++ imports/ui/navigation/forms/SignInForm.jsx
@@ -1,49 +1,86 @@
-import React, { Component, PropTypes } from 'react';
-import Dialog from 'material-ui/Dialog';
-import FlatButton from 'material-ui/FlatButton';
-import TextField from 'material-ui/TextField';
+import React, {Component, PropTypes} from "react";
+import Dialog from "material-ui/Dialog";
+import FlatButton from "material-ui/FlatButton";
+import FormsyText from "formsy-material-ui/lib/FormsyText";
+
+const errorMessages = {
+    loginError: {
+        isEmail: "Provide valid email"
+    },
+    passwordError: {
+        minLength: "Password requires min. 6 characters",
+        isValidPassword: "Password not valid"
+    }
+};
+
+const validations = {
+    login: {
+        isEmail: true,
+    },
+    password: {
+        minLength: 6,
+        isValidPassword: function (values, value) {
+            return true;
+        }
+    }
+}
 
 class SignInForm extends Component {
-    
-    constructor(props){
+
+    constructor(props) {
         super(props);
         this.state = {
-          open: false,
-          emailValue: "",
-          pswValue:   ""  
+            open: false,
+            emailValue: "",
+            pswValue: "",
+            canSubmit: false,
         };
-        this.handleOpen =           this.handleOpen.bind(this);
-        this.handleClose =          this.handleClose.bind(this);
-        this.handleLogging =        this.handleLogging.bind(this);
-        this.handleEmailChange =    this.handleEmailChange.bind(this);
-        this.handlePswChange =      this.handlePswChange.bind(this);
+        this.enableButton = this.enableButton.bind(this);
+        this.disableButton = this.disableButton.bind(this);
+        this.handleOpen = this.handleOpen.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.handleLogging = this.handleLogging.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePswChange = this.handlePswChange.bind(this);
     }
-    
-    handleLogging(){
+
+    enableButton() {
+        this.setState({
+            canSubmit: true,
+        });
+    }
+
+    disableButton() {
+        this.setState({
+            canSubmit: false,
+        });
+    }
+
+    handleLogging() {
         this.handleClose();
     }
-    
-    handleOpen(){
+
+    handleOpen() {
         this.setState({
             open: true
         });
     }
-    
-    handleClose(){
+
+    handleClose() {
         this.setState({
             open: false,
             emailValue: "",
-            pswValue:   ""   
+            pswValue: ""
         });
     }
 
-    handleEmailChange( eventObject ){
+    handleEmailChange(eventObject) {
         this.setState({
             emailValue: eventObject.target.value
         });
     }
-    
-    handlePswChange( eventObject ){
+
+    handlePswChange(eventObject) {
         this.setState({
             pswValue: eventObject.target.value
         });
@@ -54,38 +91,60 @@ class SignInForm extends Component {
             (<FlatButton
                 label="Cancel"
                 primary
-                onTouchTap={this.handleClose} 
+                onTouchTap={this.handleClose}
             />),
             (<FlatButton
+                type="submit"
                 label="Login"
+                disabled={!this.state.canSubmit}
                 primary
                 keyboardFocused
-                onTouchTap={this.handleLogging} 
+                onTouchTap={this.handleLogging}
             />)
         ];
         return (
             <div>
-                <Dialog 
+                <Dialog
                     title="Signing in"
                     actions={actions}
                     modal={false}
                     open={this.state.open}
-                    onRequestClose={this.handleClose} 
+                    onRequestClose={this.handleClose}
                 >
-                    <TextField
-                        hintText="E-mail"
-                        floatingLabelText="E-mail"
-                        value={this.emailValue}
-                        onChange={this.handleEmailChange}
-                    />
-                    <br />
-                    <TextField
-                        hintText="Password"
-                        floatingLabelText="Password"
-                        type="password"
-                        value={this.pswValue}
-                        onChange={this.handlePswChange}
-                    />
+                    <Formsy.Form
+                        onValid={this.enableButton}
+                        onInvalid={this.disableButton}
+                    >
+                        <div className="row between-xs">
+                            <div className="col-md auto-width">
+                                <FormsyText
+                                    name="email"
+                                    className="box auto-width"
+                                    hintText="E-mail"
+                                    floatingLabelText="E-mail"
+                                    required
+                                    validations={validations.login}
+                                    validationErrors={errorMessages.loginError}
+                                    value={this.state.emailValue}
+                                    onChange={this.handleEmailChange}
+                                />
+                            </div>
+                            <div className="col-md auto-width">
+                                <FormsyText
+                                    name="password"
+                                    className="box auto-width"
+                                    hintText="Password"
+                                    floatingLabelText="Password"
+                                    type="password"
+                                    required
+                                    validations={validations.password}
+                                    validationErrors={errorMessages.passwordError}
+                                    value={this.state.pswValue}
+                                    onChange={this.handlePswChange}
+                                />
+                            </div>
+                        </div>
+                    </Formsy.Form>
                 </Dialog>
             </div>
         );
