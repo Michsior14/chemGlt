a590bba88498b611a27ce21ec26f138385d10304
Michał Mrozek <michal.mrozek@abiserwkom.eu>
2016-08-28 13:47:27 +0200

    REFACTOR: redux forms added into singinForm

--- imports/ui/navigation/forms/SignInForm.jsx
+++ imports/ui/navigation/forms/SignInForm.jsx
@@ -1,47 +1,29 @@
 import React, {Component, PropTypes} from "react";
-import { connect } from 'react-redux';
+import {connect} from "react-redux";
 import Dialog from "material-ui/Dialog";
 import FlatButton from "material-ui/FlatButton";
-import FormsyText from "formsy-material-ui/lib/FormsyText";
-import validations from "/lib/validations";
-import messages from "/lib/messages";
-
-import { signIn, closeDialog, updateSignInFields } from "../../../../lib/actions/account";
+import {reduxForm, Field} from "redux-form";
+import {TextField} from "redux-form-material-ui";
+import {closeDialog} from "/lib/actions/account";
+import {validateSignIn} from "/lib/validations";
 
 
 class SignInForm extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            // open: false,
-            emailValue: "",
-            pswValue: "",
-            canSubmit: false,
-        };
-        this.enableButton = this.enableButton.bind(this);
-        this.disableButton = this.disableButton.bind(this);
     }
-
-    enableButton() {
-        this.setState({
-            canSubmit: true,
-        });
-    }
-
-    disableButton() {
-        this.setState({
-            canSubmit: false,
-        });
-    }
-
 
     render() {
+        const {
+            handleClose, submitting,
+            open, handleSubmit, valid
+        } = this.props;
         const actions = [
             (<FlatButton
                 label="Cancel"
                 primary
-                onTouchTap={this.handleClose}
+                onTouchTap={handleClose}
             />),
             (<FlatButton
                 type="submit"
@@ -49,6 +31,7 @@ class SignInForm extends Component {
                 primary
                 keyboardFocused
                 form="loginForm"
+                disabled={!valid || submitting}
             />)
         ];
         return (
@@ -57,50 +40,35 @@ class SignInForm extends Component {
                     title="Signing in"
                     actions={actions}
                     modal={false}
-                    open={this.props.open}
-                    onRequestClose={this.props.handleClose}
+                    open={open}
+                    onRequestClose={handleClose}
                 >
-                    <Formsy.Form
+                    <form
                         id="loginForm"
-                        onSubmit={() => {
-                            this.props.handleLogging(
-                                this.props.emailValue,
-                                this.props.pswValue
-                            );
-                        }}
+                        onSubmit={handleSubmit}
                     >
                         <div className="row between-xs">
                             <div className="col-md auto-width">
-                                <FormsyText
+                                <Field
                                     name="email"
+                                    component={TextField}
                                     className="box auto-width"
                                     hintText="E-mail"
                                     floatingLabelText="E-mail"
-                                    value={this.props.emailValue}
-                                    onChange={
-                                        (eventObject) => {
-                                            this.props.handleEmailChange(eventObject, this.props.pswValue)
-                                        }
-                                    }
                                 />
                             </div>
                             <div className="col-md auto-width">
-                                <FormsyText
+                                <Field
                                     name="password"
+                                    component={TextField}
                                     className="box auto-width"
                                     hintText="Password"
                                     floatingLabelText="Password"
                                     type="password"
-                                    value={this.props.pswValue}
-                                    onChange={
-                                        (eventObject) => {
-                                            this.props.handlePswChange(eventObject, this.props.emailValue)
-                                        }
-                                    }
                                 />
                             </div>
                         </div>
-                    </Formsy.Form>
+                    </form>
                 </Dialog>
             </div>
         );
@@ -111,35 +79,31 @@ class SignInForm extends Component {
 const mapStateToProps = (state, ownProps) => {
     const reducer = state.accountReducer;
     return {
-        open:       (reducer.isDialog && reducer.openedDialog === 'SIGN_IN'),
-        emailValue: reducer.isDialog ? reducer.fieldsDialog.email : null,
-        pswValue:   reducer.isDialog ? reducer.fieldsDialog.password : null
+        open: (reducer.isDialog && reducer.openedDialog === 'SIGN_IN'),
+        email: reducer.isDialog ? reducer.fieldsDialog.email : null,
+        password: reducer.isDialog ? reducer.fieldsDialog.password : null
     }
-}
+};
 
 const mapDispatchToProps = (dispatch, ownProps) => {
     return {
         handleClose: () => {
             dispatch(closeDialog());
-        },
-        handleEmailChange: ( eventObject, password ) => {
-            dispatch(updateSignInFields({
-                email:      eventObject.target.value,
-                password:   password
-            }));
-        },
-        handlePswChange: ( eventObject, email ) => {
-            dispatch(updateSignInFields({
-                email:      email,
-                password:   eventObject.target.value
-            }));
-        },
-        handleLogging: (email, password) => {
-            console.log(email);
-            console.log(password);
         }
     }
-}
+};
+
+SignInForm = reduxForm({
+    form: 'SignInForm',
+    onSubmit: (data, dispatch) => {
+        console.log(data.email);
+        console.log(data.password);
+    },
+    onSubmitSuccess: (result, dispatch) => {
+        dispatch(closeDialog());
+    },
+    validate: validateSignIn
+})(SignInForm);
 
 SignInForm = connect(
     mapStateToProps,
