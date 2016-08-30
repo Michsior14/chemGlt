8379fafae3467aa53ea37d7261913223d035ed6c
MKMZ <rafal.klimek@student.uj.edu.pl>
2016-08-30 16:09:31 +0200

    FEATURE: dropping files in GraphList

--- imports/ui/GraphList.jsx
+++ imports/ui/GraphList.jsx
@@ -1,8 +1,9 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import DropZone from 'react-dropzone';
+
 import { Table, TableBody, TableHeader, TableHeaderColumn } from 'material-ui/Table';
 import { TableRow, TableRowColumn } from 'material-ui/Table';
-
 
 
 const mapStateToProps = (state, ownProps) => {
@@ -15,7 +16,10 @@ const mapStateToProps = (state, ownProps) => {
 const mapDispatchToProps = (dispatch, ownProps) => {
     return {
         handlers: {
-
+        	onGraphDrop: ( files ) => {
+        		console.log('Files:');
+        		console.log(files);
+        	}
         }
     }
 }
@@ -23,6 +27,12 @@ const mapDispatchToProps = (dispatch, ownProps) => {
 let  GraphList = ({ handlers }) => {
 	return (
 		<div>
+			<DropZone onDrop={handlers.onGraphDrop}>
+				<div>
+					Drop your data files to create graphs.
+				</div>
+			</DropZone>
+			<br/>
 			<Table>
 				<TableHeader>
 					<TableRow>
