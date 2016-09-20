0569b60c6726b16e70decd04756d3789e57f0799
Michał Mrozek <michalmrozek@wp.pl>
2016-09-20 20:04:07 +0200

    REFACTOR: fix eslint autofix on utilities

--- imports/helpers/utilities.js
+++ imports/helpers/utilities.js
@@ -1,32 +1,30 @@
 
 
 const utilities = {
-    										manageAsyncTasks: (dataArray, asyncFunc, callback) => {
-        										let counter = 0;
-        										let resultArray = new Array(dataArray.length);
+	manageAsyncTasks: (dataArray, asyncFunc, callback) => {
+		let counter = 0;
+		let resultArray = new Array(dataArray.length);
 
-        										dataArray.forEach((item, idx) => {
-            										asyncFunc(item, idx, ( result ) => {
-                										resultArray[idx] = result;
-                										counter += 1;
+		dataArray.forEach((item, idx) => {
+			asyncFunc(item, idx, (result) => {
+				resultArray[idx] = result;
+				counter += 1;
 
-                										if (counter === dataArray.length){
-                    										callback(resultArray);
-                }
-            });
-
-        });
-
-    },
-    										getNow: () => {
-        										return new Date(Date.now()).toLocaleString();
-    },
-    										randomColor: () => {
-        										return "rgba(" + 
-				Math.floor(Math.random() * 255) + ", " + 
-				Math.floor(Math.random() * 255) + ", " + 
-				Math.floor(Math.random() * 255) + ", 1)";
-    }
+				if (counter === dataArray.length) {
+					callback(resultArray);
+				}
+			});
+		});
+	},
+	getNow: () => {
+		return new Date(Date.now()).toLocaleString();
+	},
+	randomColor: () => {
+		return "rgba(" +
+			Math.floor(Math.random() * 255) + ", " +
+			Math.floor(Math.random() * 255) + ", " +
+			Math.floor(Math.random() * 255) + ", 1)";
+	}
 };
 
 export default utilities;
