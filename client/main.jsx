f78f5a627ade0a7def3bd081d4529060c32b94a4
MKMZ <rafal.klimek@student.uj.edu.pl>
2016-09-21 13:48:38 +0200

    REFACTOR: minor changes

--- client/main.jsx
+++ client/main.jsx
@@ -1,16 +1,8 @@
 import {Meteor} from "meteor/meteor";
 import React from "react";
 import { render } from 'react-dom';
-// import {DocHead} from "meteor/kadira:dochead"
 
 import Root from "/imports/ui/Root"
-
-
-// const metaInfo = {
-//     name: "viewport",
-//     content: "width=device-width, initial-scale=1.0"
-// };
-// DocHead.addMeta(metaInfo);
 
 Meteor.startup(() => {
   render(<Root />, document.getElementById('render-target'));
